eeebb42853dce487d9755ad61577298d9ca3dcf6
lala040221 <F74111097@gs.ncku.edu.tw>
2026-01-30 19:15:27 +0800

    Update schedule to 8:01 Tue/Fri/Sun and refine auto booking logic

--- auto_book.py
+++ auto_book.py
@@ -9,7 +9,7 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 BASE_URL = "https://pub-tyn-reha-shihao.leaftech.tw/"
-TIME_SLOTS = ["11:30", "18:00"]          # 你要嘗試的時段
+TIME_SLOTS = ["11:15", "17:45"]          # 你要嘗試的時段
 VALID_WEEKDAYS = {1, 3, 5}               # Tue/Thu/Sat (Mon=0)
 
 
